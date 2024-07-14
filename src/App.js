d8ff8be49be9d8b86ce74ec153fb59c1db46bb16
Adebemdev <adeolabusayo1995@gmail.com>
2024-07-14 16:47:46 -0700

    updating file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container bg-Green(lighter) mx-auto mt-10 p-12">
+      <Form />
     </div>
   );
 }
 
-export default App;
+function Form() {
+  return (
+    <form className="bg-white min-h-screen rounded-2xl p-10">
+      <h1 className="text-6xl text-Grey(darker) font-karla font-bold tracking-tighter mb-10">
+        Contact Us
+      </h1>
+      <div className="flex flex-col gap-4 mb-8">
+        <label className="text-3xl font-karla  text-Grey(darker) ">
+          First Name <span className="text-Green(medium) p-4">*</span>
+        </label>
+        <input
+          type="text"
+          required
+          className="border-2 rounded-2xl w-full p-6 border-gray-300 focus: outline-none hover:border-Green(lighter)"
+        />
+      </div>
+      <div className="flex flex-col gap-4 mb-8">
+        <label className="text-3xl font-karla  text-Grey(darker)">
+          Last Name <span className="text-Green(medium) p-4">*</span>
+        </label>
+
+        <input
+          type="text"
+          required
+          className="border-2 rounded-2xl w-full p-6 border-gray-300 focus: outline-none hover:border-Green(lighter)"
+        />
+      </div>
+      <div className="flex flex-col gap-4 mb-8">
+        <label className="text-3xl font-karla text-Grey(darker)">
+          Email Address <span className="text-Green(medium) p-4">*</span>
+        </label>
+        <input
+          type="text"
+          required
+          className="border-2 rounded-2xl w-full p-6 border-gray-300 focus: outline-none hover:border-Green(lighter)"
+        />
+      </div>
+      <div className="flex flex-col gap-4 mb-8">
+        <label className="text-3xl font-karla text-Grey(darker)">
+          Query Type <span className="text-Green(medium) p-4">*</span>
+        </label>
+        <div className="w-full border-2 p-4 rounded-2xl">
+          <label className="inline-flex relative items-center gap-8 text-3xl text-Grey(darker)">
+            <input type="radio" checked className="inline-block" />
+            General Enqury
+          </label>
+        </div>
+        <div className="w-full border-2 p-4 rounded-2xl">
+          <label className="inline-flex relative items-center gap-6 text-3xl text-Grey(darker) ">
+            <input type="radio" className="inline-block" />
+            Support Request
+          </label>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 mb-8">
+        <label className="text-3xl font-karla  text-Grey(darker)">
+          Messages <span className="text-Green(medium) p-4">*</span>
+        </label>
+        <textarea
+          rows="1"
+          cols="20"
+          type="text"
+          className="border-2 rounded-md w-full p-2 border-gray-300 focus: outline-none hover:border-Green(lighter)"
+        ></textarea>
+      </div>
+
+      <div className="flex gap-6 mb-6">
+        <input type="checkbox" />
+        <label className="text-2xl text-Grey(darker) tracking-tight leading-6">
+          I consent to being contacted by the team
+        </label>
+      </div>
+      <button
+        type="submit"
+        className="inline-block w-full text-white text-2xl bg-Green(medium) p-4 rounded-2xl"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
